54788b2a6d843526b3739ed4a4a10df28340f1fa
artem <artemqq5@gmail.com>
2025-01-20 14:38:02 +0200

    update limiter (topup + create) account

--- domain/handler/client/mcc/accounts/create_account_client.py
+++ domain/handler/client/mcc/accounts/create_account_client.py
@@ -1,4 +1,6 @@
 import asyncio
+import logging
+from datetime import datetime, timedelta
 
 from aiogram import Router, Bot
 from aiogram.fsm.context import FSMContext
@@ -21,26 +23,32 @@ router = Router()
 router.message.middleware(MCCLimitClientMiddleware())
 router.callback_query.middleware(MCCLimitClientMiddleware())
 
-create_account_last_call_times = []
+create_account_black_list = {}
 
 
 async def remove_limiter(team_uuid):
-    """Видалення ліміту через 5 секунди."""
-    print(f"create_account List block request before {create_account_last_call_times}")
-    print(f"create_account team remove block {team_uuid}")
+    """Видалення ліміту через REQUEST_LIMIT_SECONDS."""
+    logging.info(f"Creating account - List block request before: {create_account_black_list}")
+    logging.info(f"Removing block for team_uuid: {team_uuid}")
+
     await asyncio.sleep(REQUEST_LIMIT_SECONDS)
-    create_account_last_call_times.remove(team_uuid)  # Видаляємо ключ, якщо існує
-    print(f"create_account List block request after {create_account_last_call_times}")
+
+    if team_uuid in create_account_black_list:
+        del create_account_black_list[team_uuid]
+        logging.info(f"Creating account - Removed block for team_uuid: {team_uuid}")
+    logging.info(f"Creating account - List block request after: {create_account_black_list}")
 
 
 @router.callback_query(CreateSubAccountClient.filter())
 async def create_account(callback: CallbackQuery, state: FSMContext, i18n: I18nContext):
+    logging.info(f"Creating account - Starting new account creation process for {callback.from_user.id}")
     await state.set_state(CreateAccountClientState.Name)
     await callback.message.edit_text(i18n.CLIENT.ACCOUNT.CREATE.NAME(), reply_markup=kb_back_detail_mcc)
 
 
 @router.message(CreateAccountClientState.Name)
 async def create_name_save(message: Message, state: FSMContext, i18n: I18nContext, bot: Bot):
+    logging.info(f"Creating account - Name provided: {message.text}")
     if len(message.text) > 255:
         await message.answer(
             i18n.CLIENT.ACCOUNT.CREATE.NAME.ERROR(len=len(message.text)), reply_markup=kb_back_detail_mcc)
@@ -92,34 +100,33 @@ async def create_timezone_save(message: Message, state: FSMContext, i18n: I18nCo
 
     data = await state.get_data()
     mcc = MCCRepository().mcc_by_uuid(data['mcc_uuid'])
-
     access = AccessRepository().access_by_user_id(message.from_user.id)
 
-    print(f"create_timezone_save access {access}")
-    print(f"create_timezone_save balance {BalanceRepository().balance(data['mcc_uuid'], data['team_uuid'])}")
+    logging.info(f"Creating account - Access: {access}")
+    logging.info(f"Creating account - Balance: {BalanceRepository().balance(data['mcc_uuid'], data['team_uuid'])}")
 
-    # Перевіряємо, чи вже був запит
     team_uuid = access['team_uuid']
-    if team_uuid in create_account_last_call_times:
-        print(f"create_timezone_save List block if reject request {create_account_last_call_times}")
-        print(f"create_timezone_save team uuid if reject request {access['team_name']}")
-        await message.answer(i18n.CLIENT.WAIT_FOR_REQUEST())
-        return
+    current_time = datetime.now()
 
-    # Встановлюємо обмеження на запит
-    create_account_last_call_times.append(team_uuid)
+    if team_uuid in create_account_black_list:
+        last_call_time = create_account_black_list[team_uuid]
+        if current_time - last_call_time < timedelta(seconds=60):
+            logging.warning(f"Creating account - Request blocked for team {team_uuid} \n{create_account_black_list}")
+            await message.answer(i18n.CLIENT.WAIT_FOR_REQUEST())
+            return
 
-    # обнуляємо стан FSM
+    create_account_black_list[team_uuid] = current_time
+
     await state.set_state(None)
 
-    # check value balance
+    # Перевірка балансу
     balance = BalanceRepository().balance(data['mcc_uuid'], data['team_uuid'])
     if balance['balance'] < data['amount']:
         await message.answer(i18n.CLIENT.ACCOUNT.CREATE.AMOUNT.NOMONEY(balance=balance['balance']),
                              reply_markup=kb_back_detail_mcc)
         return
 
-    # Try Authorizate MCC API
+    # Спроба авторизації через MCC API
     auth = YeezyAPI().generate_auth(mcc['mcc_id'], mcc['mcc_token'])
 
     if not auth:
@@ -129,7 +136,7 @@ async def create_timezone_save(message: Message, state: FSMContext, i18n: I18nCo
         )
         return
 
-    # create account by API
+    # Створення облікового запису через API
     def create_account_api():
         return YeezyAPI().create_account(auth['token'], data['email'], data['amount'], timezone)
 
@@ -139,14 +146,15 @@ async def create_timezone_save(message: Message, state: FSMContext, i18n: I18nCo
 
     asyncio.create_task(remove_limiter(team_uuid))
 
-    # transaction create account
+    # Якщо транзакція не була успішною
     if not create_account_transaction['result']:
+        logging.error(f"Creating account - Error creating account for {data['email']}")
         await message.answer(i18n.CLIENT.ACCOUNT.CREATE.FAIL(), reply_markup=kb_back_detail_mcc)
         await NotificationAdmin.user_create_account_error(message.from_user.id, bot, i18n, data,
                                                           create_account_transaction['error'])
         return
 
-    # All creation account success
+    # Успішне створення акаунту
     await message.answer(i18n.CLIENT.ACCOUNT.CREATE.SUCCESS(
         mcc_name=mcc['mcc_name']), reply_markup=kb_back_detail_mcc
     )
